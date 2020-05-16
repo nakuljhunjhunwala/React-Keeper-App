0c4760843ecab3c890a336b6d8bea01d49f23724
unknown <jhunjhunwalanakul@gmail.com>
2020-05-17 01:48:30 +0530

    V.1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+
+  const d = new Date();
+  const year = d.getFullYear();
+
+  return( <footer>
+<p>
+Copyright {year}
+</p>
+  </footer>
+)
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Heading() {
+  return (<div className="header">
+<h1>Keeper</h1>
+  </div>)
+}
+
+export default Heading;
